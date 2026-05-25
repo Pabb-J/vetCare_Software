4ecf2a0cef75ad3d5350cfd8463dd814dcfdd1a1
Pabb-J <89533171+Pabb-J@users.noreply.github.com>
2026-05-25 18:30:36 -0300

    inicializar mail

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,8 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
+    mail.init_app(app)
+
 
     from app.models.mascota import Mascota
 
