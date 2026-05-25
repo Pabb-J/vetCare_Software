6ee254063081dd70a2b1e6e934c92b1e86ea3728
Pabb-J <89533171+Pabb-J@users.noreply.github.com>
2026-05-25 18:06:48 -0300

    Integración para que la app envie mail de verdad
    Las rutas de recuperación de contraseña ahora pueden enviar correos de verdad para validacion

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 ﻿from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -24,5 +25,15 @@ def create_app():
     from app.routes.turnos import turnos
     app.register_blueprint(turnos)
 
-    return app
+    # Configuración de correo
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'tu_correo@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'tu_app_password'
+    app.config['MAIL_DEFAULT_SENDER'] = 'tu_correo@gmail.com'
 
+    from app import mail
+    mail.init_app(app)
+
+    return app
