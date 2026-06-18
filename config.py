2082f390f1fd8008cf91abba55dd0918d37a3fdd
brian gomez <briangenio1@gmail.com>
2026-06-18 01:10:25 -0300

    Agregar config para deploy en Render (PostgreSQL + gunicorn)

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'vetcare_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///vetcare.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'vetcare_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///vetcare.db').replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
